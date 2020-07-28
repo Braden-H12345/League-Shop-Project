e2e26751bb26d93086943a0000a629854eeff183
Braden Honea <bradenhonea@yahoo.com>
2020-07-28 02:20:40 -0500

    Added Gold controller items started as scriptable objects for further implementation
    TO DO: 
    create shop itself, and input functionality of buttons/display stats when left click items

---
+++ Assets/Scripts/GoldControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldControl : MonoBehaviour
+{
+    [SerializeField] Text goldDisplay = null;
+    [SerializeField] int GoldAtStart = 0;
+    [SerializeField] int goldIncrease = 0;
+    [SerializeField] int goldDeacrease = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GoldAtStart += goldIncrease;
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            GoldAtStart -= goldDeacrease;
+        }
+
+        goldDisplay.text = "Gold: " + GoldAtStart;
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemArt;
+
+    public int goldCost;
+    public int sellAmount;
+
+    //Stats
+    public int AD;
+    public int AP;
+    public int armor;
+    public int magic_resist;
+    public int health;
+    public int movespeedFlat;
+    public int movespeedPercent;
+    public int crit_chance;
+    public int attack_speed;
+    public int mana;
+    public int cooldown_reduce;
+}
